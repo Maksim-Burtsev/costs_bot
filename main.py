2bf5b46249ee724d9dec7c3f7073149945c07612
MaximBurtsev <zadrot-lol@list.ru>
2022-05-15 22:27:11 +0300

    refactoring

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import telebot
 from telebot import types
 from dotenv import load_dotenv
 
-from services import get_static, add_buy
+from services import Service
 
 
 load_dotenv()
@@ -13,6 +13,7 @@ TOKEN = os.getenv('TOKEN')
 MY_ID = int(os.getenv('MY_ID'))
 
 bot = telebot.TeleBot(TOKEN)
+service = Service()
 
 
 @bot.message_handler(commands=['start'])
@@ -31,11 +32,11 @@ def main(message):
     if message.chat.id == MY_ID:
         if message.text == 'Статистика за неделю':
             week_number = date.today().isocalendar().week
-            message_text = get_static(week_number=week_number)
+            message_text = service.get_static(week_number=week_number)
             bot.send_message(message.chat.id, message_text)
 
         elif message.text == "Отчёт за всё время":
-            message_text = get_static()
+            message_text = service.get_static()
             bot.send_message(message.chat.id, message_text)
         else:
             text = message.text
@@ -43,12 +44,12 @@ def main(message):
             if len(text) == 2 and text[0].isalpha() and text[1].isdigit():
                 name = text[0]
                 price = int(text[1])
-                add_buy(name, price)
+                service.add_buy(name, price)
                 bot.send_message(message.chat.id, 'Accepted')
             elif len(text) == 2 and text[1].isalpha() and text[0].isdigit():
                 name = text[1]
                 price = int(text[0])
-                add_buy(name, price)
+                service.add_buy(name, price)
                 bot.send_message(message.chat.id, 'Accepted')
             else:
                 bot.send_message(message.chat.id, 'Dismissed')
--- services.py
+++ services.py
@@ -6,75 +6,67 @@ from tabulate import tabulate
 from dotenv import load_dotenv
 
 
-load_dotenv()
-URL_GET = os.getenv('URL_GET')
-URL_CREATE = os.getenv('URL_CREATE')
+class Service:
 
+    def __init__(self) -> None:
+        load_dotenv()
+        self.URL_GET = os.getenv('URL_GET')
+        self.URL_CREATE = os.getenv('URL_CREATE')
 
-def _get_data_from_api(month=None, week_number=None) -> list:
-    """
-    Делает запрос к API и получает данные
-    """
-    if month:
-        response = requests.get(f'{URL_GET}?month={month}')
-    elif week_number:
-        response = requests.get(f'{URL_GET}?week_number={week_number}')
-    else:
-        response = requests.get(URL_GET)
+    def _get_data_from_api(self, month=None, week_number=None) -> tuple[list, int]:
+        """
+        Делает запрос к API и возвращает отсортированные данные с общей суммой
+        """
+        if month:
+            response = requests.get(f'{self.URL_GET}?month={month}')
+        elif week_number:
+            response = requests.get(
+                f'{self.URL_GET}?week_number={week_number}')
+        else:
+            response = requests.get(self.URL_GET)
 
-    data = json.loads(response.text)
-    data = [i for i in sorted(data, key=lambda x:x['cost'], reverse=True)]
+        data = json.loads(response.text)
+        data = [i for i in sorted(data, key=lambda x:x['cost'], reverse=True)]
 
-    total = _get_total_spent(data)
+        total = self._get_total_spent(data)
 
-    return (data, total)
+        return (data, total)
 
+    def _get_total_spent(self, data: list[dict]) -> int:
+        """
+        Считает общую сумму покупок
+        """
+        return sum([buy['cost'] for buy in data])
 
-def _get_total_spent(data: list[dict]) -> int:
-    """
-    Считает общую сумму покупок
-    """
-    return sum([buy['cost'] for buy in data])
+    def _format_message(self, data: list, total: int) -> str:
+        """
+        Форматирует сообщение для отправки
+        """
+        data_list = [[i['name'], f"{i['cost']} руб."] for i in data]
 
+        res = tabulate(data_list, tablefmt='tsv')
 
-def _format_message(data: list, total: int) -> str:
-    """
-    Форматирует сообщение для отправки
-    """
+        res += '\n' + '—'*17 + f'\nИТОГ {total} руб.'
+        return res
 
-    data_list = [[i['name'], f"{i['cost']} руб."] for i in data]
+    def get_static(self, month=None, week_number=None) -> str:
+        """
+        Возвращает список покупок за месяц и считает сумму
+        """
+        data, total = self._get_data_from_api(month, week_number)
 
-    res = tabulate(data_list, tablefmt='tsv')
+        return self._format_message(data, total)
 
-    res += '\n' + '—'*17 + f'\nИТОГ {total} руб.'
-    return res
+    def add_buy(self, name: str, price: int) -> bool:
+        """
+        Делает запрос для добавления данных
+        """
+        data = {
+            'name': name,
+            'price': price
+        }
 
-
-def get_static(month=None, week_number=None) -> str:
-    """
-    Возвращает список покупок за месяц и считает сумму
-    """
-    data, total = _get_data_from_api(month, week_number)
-
-    return _format_message(data, total)
-
-
-def add_buy(name: str, price: int) -> bool:
-    """
-    Делает запрос для добавления данных
-    """
-
-    data = {
-        'name': name,
-        'price': price
-    }
-
-    response = requests.post(URL_CREATE, json=data)
-    if response.status_code == 201:
-        return True
-    return False
-
-
-if __name__ == '__main__':
-    print(get_static(week_number=17))
-    add_buy('амилопептин', 500)
+        response = requests.post(self.URL_CREATE, json=data)
+        if response.status_code == 201:
+            return True
+        return False
